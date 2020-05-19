766d9225dacfbfa00f957fa4b6126162e5f703fc
Anton Pedan <caravellejetta@gmail.com>
2020-05-19 22:11:23 +0300

    story2211

---
+++ hillel-react/dz1/my-app/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+import './content.txt'
+
+class Magic extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Пожалуйста напишите желание'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Сим салабим: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Джин:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Исполнить" />
+      </form>
+    );
+  }
+}
+
+
+export default Magic;
