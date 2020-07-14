dc612017f46b6d50d5ede7568cc8333b6c13f25a
Anton Pedan <caravellejetta@gmail.com>
2020-07-14 13:49:12 +0300

    архитектура

---
+++ hillel-react/dz7/admin-pannel/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  Link,
+} from 'react-router-dom';
+import Home from './components/Home';
+import Container from '@material-ui/core/Container/Container';
+
+function App() {
+  return (
+    <Router>
+      <Container maxWidth="sm">
+        
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
---
+++ hillel-react/dz7/admin-pannel/src/components/UsersList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+
+function UsersList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UsersList
