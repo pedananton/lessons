10657e48c9375eba71603f67544a9cd931427041
Anton Pedan <caravellejetta@gmail.com>
2020-07-14 14:10:31 +0300

    1410

--- hillel-react/dz7/admin-pannel/src/App.js
+++ hillel-react/dz7/admin-pannel/src/App.js
@@ -10,12 +10,25 @@ import {
 } from 'react-router-dom';
 import Home from './components/Home';
 import Container from '@material-ui/core/Container/Container';
+import Typography from '@material-ui/core/Typography';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 function App() {
   return (
     <Router>
-      <Container maxWidth="sm">
-        
+      <Container maxWidth="lg">
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} >
+        <Menu
+        id="simple-menu"
+        open={Boolean()}
+
+      >
+        <MenuItem >Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
+        </Typography>
       </Container>
     </Router>
   );
